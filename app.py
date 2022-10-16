4c8b17019297a85021e51ced19405ee588dfd218
Sreeja Bhattacharya <sreejabhattacharya@Sreejas-MacBook-Air.local>
2022-10-16 11:15:43 +0530

    modified app.py

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def update(id):
         return render_template('update.html', task = task)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
